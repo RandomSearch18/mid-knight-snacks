cb8e11ed65eb6f8f5fcb5331234f3f3123ab367a
RandomSearch18 <101704343+RandomSearch18@users.noreply.github.com>
2024-11-23 15:17:52 +0000

    Tiles and stuff!
    Co-authored-by: AndyPandy40 <AndyPandy40@users.noreply.github.com>

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Counterspell 2024
 
 Mish, Andrew and Morgan
+
+## How to run the game
+
+1. Clone the repository
+2.
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from math import ceil
 from pathlib import Path
 
+tile_size = 64
+
 
 class GameConfig:
     WINDOW_WIDTH = 1000
@@ -31,26 +33,71 @@ class GameBackground(Drawable):
                 screen.blit(self.surface, (col * tile_height, row * tile_width))
 
 
+class Player(Drawable):
+    def __init__(self):
+        self.x = 50
+        self.y = 800 - (50 * 2 + 50)
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.weight = 1
+        self.width = 50
+        self.height = 50
+
+    def draw(self, screen: pygame.surface.Surface):
+        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, self.width, self.height))
+
+    def is_on_ground(self, tilemap):
+        bottom = self.y + self.height
+        tilemap_row = bottom // tile_size
+        tilemap_col = self.x // tile_size
+        return tilemap[tilemap_row][tilemap_col] == 1
+
+    def tick(self, game):
+        self.x += self.velocity_x
+        self.y += self.velocity_y
+        if not self.is_on_ground(game.level.tilemap):
+            self.velocity_y += self.weight
+        else:
+            self.velocity_y = 0
+
+
 class Game:
     def __init__(self):
         self.config = GameConfig()
         self.window = pygame.display.set_mode(
             (self.config.WINDOW_WIDTH, self.config.WINDOW_HEIGHT)
         )
-        self.drawables = [GameBackground()]
+        self.player = Player()
+        self.drawables = [self.player]
         self.clock = pygame.time.Clock()
+        self.level = Level1()
 
     def tick(self):
         # Event handling!
         for event in pygame.event.get():
+            base_speed = 5
             if event.type == pygame.QUIT:
                 running = False
                 pygame.quit()
                 # sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    self.player.velocity_x = -base_speed
+                elif event.key == pygame.K_d:
+                    self.player.velocity_x = base_speed
+                elif event.key == pygame.K_SPACE:
+                    self.player.velocity_y = -20
+            elif event.type == pygame.KEYUP:
+                if event.key in [pygame.K_a, pygame.K_d]:
+                    self.player.velocity_x = 0
 
         # Update data!
+        to_tick = [self.player]
+        for tickable in to_tick:
+            tickable.tick(self)
 
         # Update the screen!
+        self.level.draw_tilemap(self.window)
         for drawable in self.drawables:
             drawable.draw(self.window)
         pygame.display.update()
@@ -66,9 +113,54 @@ class Game:
         self.main_loop()
 
 
-class Map:
-    def __init_(self):
-        pass
+class Level1:
+    def __init__(self):
+        # black-ignore
+
+        # This displays the castle tiles where a 1 is and a blank tile where 0 is
+        self.tilemap = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ]
+
+        # Loading images
+        self.castle_tile = pygame.image.load("assets/castle_tile.png")
+        self.blank = pygame.image.load("assets/blank.jpg")
+        self.beef_tile = pygame.image.load("assets/beef_tile.png")
+
+        # Resizing images
+        self.castle_tile = pygame.transform.scale(
+            self.castle_tile, (tile_size, tile_size)
+        )
+        self.blank = pygame.transform.scale(self.blank, (tile_size, tile_size))
+        self.beef_tile = pygame.transform.scale(self.beef_tile, (tile_size, tile_size))
+
+        # A dictionary to link the numbers to the image files
+        self.tile_images = {
+            0: self.blank,
+            1: self.castle_tile,
+            2: self.beef_tile,
+        }
+
+    def draw_tilemap(self, screen):
+        # Iterates through each element in the 2d array
+        for row in range(len(self.tilemap)):
+            for col in range(len(self.tilemap[row])):
+                # Finds the tile type at a position (1 or 0)
+                tile_type = self.tilemap[row][col]
+                # Matches it with the image using the dictionary
+                tile_image = self.tile_images[tile_type]
+                # Displays them in order using their position in the array and size
+                screen.blit(tile_image, (col * tile_size, row * tile_size))
 
 
 pygame.init()
